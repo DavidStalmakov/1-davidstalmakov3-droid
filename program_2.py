bad009aeb8289912fdd1cd870d0d1d62e8127b8e
DavidStalmakov <dmstalmakov@students.unwsp.edu>
2025-09-05 21:44:23 -0500

    Small changes
    Author, date, and title

--- program_2.py
+++ program_2.py
@@ -1,5 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
+    #David Stalmakov, 9/5/2025 Assignment 1
     print('“Do you see a man skillful in his work?')
     print('He will stand before kings;')
     print('he will not stand before obscure men.”')
