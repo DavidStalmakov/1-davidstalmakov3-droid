987d34367c13bd147449af3b4c9b315a1775df07
DavidStalmakov <dmstalmakov@students.unwsp.edu>
2025-09-11 10:54:44 -0500

    Assignment

--- program_1.py
+++ program_1.py
@@ -2,6 +2,6 @@ def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
     #David Stalmakov, 9/5/2025 Assignment 1
-    print('Hello World')
+    print('  Hello World')
 
 hello_world()
--- program_2.py
+++ program_2.py
@@ -3,7 +3,7 @@ def proverbs_22_29():
     #David Stalmakov, 9/5/2025 Assignment 1
     print('“Do you see a man skillful in his work?')
     print('He will stand before kings;')
-    print('he will not stand before obscure men.”')
+    print('He will not stand before obscure men.”')
     print('     (Proverbs 22:29)')
     
 
